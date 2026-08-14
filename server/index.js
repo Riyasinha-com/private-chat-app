f7ce9abe1d423fed6786f1b0b180ba3d47457b1e
Riyasinha-com <riyasinha12225@gmail.com>
2026-08-14 20:26:52 +0530

    Set up basic backend server with Express and Socket.IO

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // we'll restrict this later for security
+  }
+});
+
+// Basic test route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Handle new connections
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
